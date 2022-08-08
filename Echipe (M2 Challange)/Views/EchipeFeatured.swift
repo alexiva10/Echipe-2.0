d254654e88354df048611105e22abf5d2c7ce8ff
alexiva10 <alexbogdanivanescu@gmail.com>
2022-08-08 18:08:39 +0300

    Build Featured View

--- Echipe (M2 Challange)/Views/EchipeFeatured.swift
+++ Echipe (M2 Challange)/Views/EchipeFeatured.swift
@@ -12,12 +12,48 @@ struct EchipeFeatured: View {
     @EnvironmentObject var model:EchipaModel
     
     var body: some View {
-        Text("Da")
+        
+        VStack(alignment: .leading, spacing: 0) {
+            
+            Text("Best team").padding(.leading).padding(.top, 40).font(.largeTitle)
+            
+            GeometryReader { geo in
+                
+                TabView {
+                    
+                    ForEach (0..<model.echipe.count) { index in
+                        if model.echipe[index].name == "Real Madrid" {
+                            
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    
+                                VStack(spacing: 0) {
+                                    Image("Real Madrid").resizable().aspectRatio( contentMode: .fill).clipped()
+                                    Text("Kings").padding(5)
+                                }
+                            }.frame(width: geo.size.width - 50, height: geo.size.height - 200, alignment: .center)
+                                .cornerRadius(15)
+                                .shadow(radius: 10)
+                                .shadow(color: .black, radius: 10, x: -5, y: 5)
+                        }
+                    }
+                    
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .automatic))
+            }
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Details")
+                
+            }.padding(.leading, .trailing)
+        }
     }
 }
 
 struct EchipeFeatured_Previews: PreviewProvider {
     static var previews: some View {
-        EchipeFeatured()
+        EchipeFeatured().environmentObject(EchipaModel())
     }
 }
