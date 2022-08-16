40afa3909bd2dc2f7bf0238a243e4cff33179d8b
alexiva10 <alexbogdanivanescu@gmail.com>
2022-08-16 12:16:04 +0300

    Update Featured View with Button

--- Echipe (M2 Challange)/Views/EchipeFeatured.swift
+++ Echipe (M2 Challange)/Views/EchipeFeatured.swift
@@ -11,28 +11,41 @@ struct EchipeFeatured: View {
     
     @EnvironmentObject var model:EchipaModel
     
+    @State var isDetailViewShowing = false
+    @State var tabSelectionIndex = 0
+    
+    
     var body: some View {
         
         VStack(alignment: .leading, spacing: 0) {
             
-            Text("Best team").bold().padding(.leading).padding(.top, 40).font(.largeTitle)
+            Text("Echipe castigatoare").bold().padding(.leading).padding(.top, 40).font(.largeTitle)
             
             GeometryReader { geo in
                 
-                TabView {
+                TabView (selection: $tabSelectionIndex) {
                     
                     ForEach (0..<model.echipe.count) { index in
-                        if model.echipe[index].name == "Real Madrid" {
+                        if model.echipe[index].trofeeUCL > 0 {
                             
-                            ZStack {
-                                Rectangle()
-                                    .foregroundColor(.white)
-                                    
-                                VStack(spacing: 0) {
-                                    Image("Real Madrid").resizable().aspectRatio( contentMode: .fill).clipped()
-                                    Text("Kings").padding(5)
+                            Button(action: {
+                                self.isDetailViewShowing = true
+                                
+                            }, label: {
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                        
+                                    VStack(spacing: 0) {
+                                        Image(model.echipe[index].image).resizable().aspectRatio( contentMode: .fill).clipped()
+                                        Text(model.echipe[index].name).padding(5)
+                                    }
                                 }
-                            }.frame(width: geo.size.width - 100, height: geo.size.height - 300, alignment: .center)
+                            }).tag(index)
+                                .sheet(isPresented: $isDetailViewShowing, content: {
+                                EchipaDetalii(echipa: model.echipe[index])
+                            })
+                                .buttonStyle(PlainButtonStyle()).frame(width: geo.size.width - 80, height: geo.size.height - 250, alignment: .center)
                                 .cornerRadius(15)
                                 .shadow(radius: 10)
                                 .shadow(color: .black, radius: 10, x: -5, y: 5)
@@ -45,11 +58,16 @@ struct EchipeFeatured: View {
             }
             
             VStack(alignment: .leading, spacing: 10) {
-                Text("Details")
+                Text("Numar trofee UCL: ").font(.headline)
+                Text(model.echipe[tabSelectionIndex].trofeeUCL)
+                
+                Text("Cei mai buni jucatori: ").font(.headline)
+                Text(model.echipe[0].jucatoriTop)
                 
             }.padding(.leading).padding(.trailing)
         }
     }
+    
 }
 
 struct EchipeFeatured_Previews: PreviewProvider {
